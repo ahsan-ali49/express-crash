ea115a89ee7d17bf31f8f2e45bcda5c1f533ed26
Ahsan Ali Shah <ahsanali.bscssef20@iba-suk.edu.pk>
2024-07-15 14:14:40 +0500

    add post form

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 const output = document.querySelector("#output");
 const button = document.querySelector("#get-posts-btn");
+const form = document.querySelector('#add-post-post');
 
 
 // Get and show posts
@@ -7,7 +8,7 @@ async function showPosts(){
     
     try {
         const res = await fetch('http://localhost:8000/api/posts');
-        if(!res.status){
+        if(!res.ok){
             throw new Error('Failed to fetch posts');
         }
 
@@ -24,5 +25,37 @@ async function showPosts(){
     }
 }
 
+// Submit new post
+async function addPost(e){
+    e.preventDefault();
+    const formData = new FormData(this);
+    const title = formData.get('title');
+
+    try {
+        const res = await fetch('http://localhost:8000/api/posts', {
+            method: 'POST',
+
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({title})
+        });
+
+        if(!res.ok){
+            throw new Error('Failed to add post')
+        }
+
+        const newPost = await res.json();
+
+        const postEl = document.createElement('div');
+        postEl.textContent = newPost.title;
+        output.appendChild(postEl);
+        showPosts()
+    } catch (error) {
+        throw new Error('Error adding post');
+    }
+}
+
 // Event Listener
 button.addEventListener('click', showPosts);
+form.addEventListener('submit', addPost);
